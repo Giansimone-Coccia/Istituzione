de53c108e99721d0f0906ed961d6e4b21b9782e8
Giansimone Coccia <S1097845@studenti.univpm.it>
2021-11-22 18:10:43 +0100

    Correction

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,28 @@
 package Main;
+import Istituzione.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		
+		
+		do {
+			System.out.println("Inserisci tipologia dipendente di cui vuoi avere informazioni");
+			System.out.println("--Dipendenti--");
+			System.out.println("1) Giornaliero");
+			System.out.println("2) Impiegato");
+			System.out.println("--------------");
+			System.out.println("3)Volontari");
+			System.out.print("Scelta: ");
+			int scelta = input.nextInt();
+			
+			switch(scelta) {
+			case 1:
+			}
+		}while();
 	}
 
 }
